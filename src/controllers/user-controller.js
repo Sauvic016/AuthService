2336e3c14a5251c0ece90c62a8c4382fa5a92002
Sauvic <sauvicpaulchy@gmail.com>
2023-01-15 21:41:33 +0530

    Implemented signup logic with password encryption

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,30 @@
+const UserService = require("../services/user-service");
+
+const userService = new UserService();
+
+const create = async (req, res) => {
+  try {
+    const response = await userService.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json({
+      data: response,
+      message: "Successfully created a user",
+      success: true,
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      message: "Something went wrong",
+      success: false,
+      err: error,
+    });
+  }
+};
+
+module.exports = {
+  create,
+};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
 
 const app = express();
 
 const prepareAndStartServer = async () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
   app.listen(PORT, () => {
     console.log(`Server started at Port: ${PORT}`);
   });
