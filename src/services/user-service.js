e2eca95e2d4b79a20ae2074a3bd04030080d4fed
Sauvic <sauvicpaulchy@gmail.com>
2023-01-16 21:14:48 +0530

    Added function to compare the incoming password with encrypted password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const UserRepository = require("../repository/user-repository");
 const { JWT_KEY } = require("../config/serverConfig");
@@ -33,6 +34,14 @@ class UserService {
       return response;
     } catch (error) {
       console.log("Something went wrong in token validation", error);
+      throw error;
+    }
+  }
+  checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+    } catch (error) {
+      console.log("Something went wrong in password comparison");
       throw error;
     }
   }
