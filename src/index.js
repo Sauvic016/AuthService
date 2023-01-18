739ea201a9fe4a4f637c9cf3bff8129912ea2337
Sauvic <sauvicpaulchy@gmail.com>
2023-01-18 23:52:16 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 
+const db = require("./models/index");
+
 const app = express();
 
 const prepareAndStartServer = async () => {
@@ -12,8 +14,11 @@ const prepareAndStartServer = async () => {
 
   app.use("/api", apiRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started at Port: ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
   });
 };
 
